3207c736799ee12dd4b7c232f01dd235853db623
Edoardo Morandi <morandidodo@gmail.com>
2024-11-19 14:58:24 +0100

    feat: improve binary stream

--- src/binary_stream.hpp
+++ src/binary_stream.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
 #include "endianess.hpp"
+#include <ios>
 
 template <typename Stream> class BinaryStream {
 public:
+  using pos_type = typename Stream::pos_type;
+  using off_type = typename Stream::off_type;
+
   BinaryStream(Stream &stream);
 
   template <typename T, typename Out = BinaryStream &>
@@ -11,6 +15,20 @@ public:
 
   template <typename T, typename Out = BinaryStream &>
   std::enable_if_t<system_is_big_endian::value, Out> operator>>(T &t);
+
+  template <typename T, typename Out = BinaryStream &>
+  std::enable_if_t<system_is_little_endian::value, Out> operator<<(T const &t);
+
+  template <typename T, typename Out = BinaryStream &>
+  std::enable_if_t<system_is_big_endian::value, Out> operator<<(T t);
+
+  bool is_open() const;
+  pos_type tellp();
+  BinaryStream<Stream> &seekp(pos_type pos);
+  BinaryStream<Stream> &seekp(off_type off, std::ios_base::seekdir dir);
+
+  Stream &inner();
+  Stream const &inner() const;
 
 private:
   Stream *stream;
--- src/binary_stream_impl.hpp
+++ src/binary_stream_impl.hpp
@@ -23,3 +23,49 @@ BinaryStream<Stream>::operator>>(T &t) {
   swapBytes(t);
   return *this;
 }
+
+template <typename Stream>
+template <typename T, typename Out>
+std::enable_if_t<system_is_little_endian::value, Out>
+BinaryStream<Stream>::operator<<(T const &t) {
+  stream->write(reinterpret_cast<char const *>(&t), sizeof(T));
+  return *this;
+}
+
+template <typename Stream>
+template <typename T, typename Out>
+std::enable_if_t<system_is_big_endian::value, Out>
+BinaryStream<Stream>::operator<<(T t) {
+  swapBytes(t);
+  stream->write(reinterpret_cast<char const *>(&t), sizeof(T));
+  return *this;
+}
+
+template <typename Stream> bool BinaryStream<Stream>::is_open() const {
+  return stream->is_open();
+}
+
+template <typename Stream> auto BinaryStream<Stream>::tellp() -> pos_type {
+  return stream->tellp();
+}
+
+template <typename Stream>
+BinaryStream<Stream> &BinaryStream<Stream>::seekp(pos_type pos) {
+  stream->seekp(pos);
+  return *this;
+}
+
+template <typename Stream>
+BinaryStream<Stream> &BinaryStream<Stream>::seekp(off_type off,
+                                                  std::ios_base::seekdir dir) {
+  stream->seekp(off, dir);
+  return *this;
+}
+
+template <typename Stream> Stream &BinaryStream<Stream>::inner() {
+  return *stream;
+}
+
+template <typename Stream> Stream const &BinaryStream<Stream>::inner() const {
+  return *stream;
+}

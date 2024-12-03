666b98df96429603e173d468d094d315a29b8c09
Edoardo Morandi <morandidodo@gmail.com>
2024-12-03 09:52:23 +0100

    feat: make mm transcript read ReadLike

--- src/mutation_map_transcript_read.hpp
+++ src/mutation_map_transcript_read.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
+#include <limits>
 #include <vector>
 
 struct MutationMapTranscriptRead {
   unsigned begin;
   unsigned end;
   std::vector<unsigned> indices;
+
+  constexpr unsigned original_begin_index() const { return begin; }
+  constexpr unsigned original_end_index() const { return end; }
+  constexpr unsigned window_begin_index() const { return 0; }
+  constexpr unsigned window_end_index() const {
+    return std::numeric_limits<unsigned>::max();
+  }
+  constexpr std::vector<unsigned> const &modifiedIndices() const {
+    return indices;
+  }
 };

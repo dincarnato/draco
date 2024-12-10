36940ef29689bfe536316f66fba1aebc6e996ac5
Edoardo Morandi <morandidodo@gmail.com>
2024-12-10 10:54:43 +0100

    feat: improve impl of parallel_reduce with no tbb
    Still a rough implementation, not particularly efficient, but at least
    it is able to handle functions that want rvalue references.

--- src/parallel/parallel_reduce.hpp
+++ src/parallel/parallel_reduce.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "defaults.hpp"
+#include <type_traits>
 
 #if USE_TBB
 
@@ -25,20 +26,40 @@ inline decltype(auto) parallel_reduce(Args &&...args) {
 namespace parallel {
 
 template <typename Range, typename Value, typename Func, typename Reduction>
-Value parallel_reduce(const Range &range, const Value &identity, Func func,
+Value parallel_reduce(const Range &range, Value &&identity, Func func,
                       const Reduction &reduction) {
-  std::vector<std::future<Value>> futures;
+  using value_t = std::remove_cvref_t<Value>;
+
+  std::vector<std::future<value_t>> futures;
   auto subRanges = range.split(threadsPerLoop, 1);
   futures.reserve(subRanges.size());
 
+  auto &&local_identity = std::forward<Value>(identity);
   for (auto subRangeIter = std::begin(subRanges);
-       subRangeIter < std::prev(std::end(subRanges)); ++subRangeIter)
-    futures.emplace_back(
-        std::async(std::launch::async, func, *subRangeIter, identity));
+       subRangeIter < std::prev(std::end(subRanges)); ++subRangeIter) {
+    if constexpr (std::is_invocable_v<Func, const Range &, value_t &&>) {
+      futures.emplace_back(std::async(std::launch::async, func, *subRangeIter,
+                                      value_t{local_identity}));
+    } else {
+      futures.emplace_back(
+          std::async(std::launch::async, func, *subRangeIter, local_identity));
+    }
+  }
 
-  Value returnValue = func(subRanges.back(), identity);
-  for (auto &&future : futures)
-    returnValue = reduction(future.get(), returnValue);
+  value_t returnValue = ([&]() {
+    if constexpr (std::is_invocable_v<Func, const Range &, value_t &&>) {
+      return func(subRanges.back(), std::move(local_identity));
+    } else {
+      return func(subRanges.back(), local_identity);
+    }
+  })();
+  for (auto &&future : futures) {
+    if constexpr (std::is_invocable_v<Reduction, value_t &&, value_t &&>) {
+      returnValue = reduction(future.get(), std::move(returnValue));
+    } else {
+      returnValue = reduction(future.get(), returnValue);
+    }
+  }
 
   return returnValue;
 }

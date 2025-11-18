4eb7a3ab2ebb1bbb863fbd8175ff82d74431dfda
Edoardo Morandi <morandidodo@gmail.com>
2025-11-18 10:23:17 +0100

    feat: add PermutationsDistances constructor
    A tool to help evaluating the distance between permutations.

--- src/clusters_replicates.hpp
+++ src/clusters_replicates.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "weighted_clusters.hpp"
+
 #include <cstddef>
 #include <cstdint>
+#include <vector>
 
 namespace clusters_replicates {
 
@@ -16,4 +19,15 @@ constexpr std::size_t distances_size(std::uint8_t replicates,
       static_cast<std::size_t>(replicates - 1) / 2;
   return replicates_combinations * clusters * clusters;
 }
+
+struct PermutationsDistances {
+  PermutationsDistances(std::vector<WeightedClusters> &replicates_clusters);
+
+private:
+  std::uint8_t replicates_;
+  std::uint8_t clusters_;
+  std::uint16_t replicates_combinations;
+  std::vector<double> distances_;
+};
+
 } // namespace clusters_replicates

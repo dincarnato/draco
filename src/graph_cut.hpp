95e9621586277ba780eff344a74935024d39be76
Edoardo Morandi <morandidodo@gmail.com>
2025-11-18 14:30:48 +0100

    feat: remove dead code

--- src/graph_cut.hpp
+++ src/graph_cut.hpp
@@ -1,23 +1,13 @@
 #pragma once
 
-#include "hard_clusters.hpp"
 #include "results/transcript.hpp"
 #include "weighted_clusters.hpp"
 #include "weighted_clusters_cluster_wrapper.hpp"
 
 #include <armadillo>
-#include <concepts>
-#include <cstdint>
-#include <utility>
-#include <variant>
 #include <vector>
 
-class RnaSecondaryStructure;
-
 class GraphCut {
-  struct FuzzyCut {};
-  struct HardCut {};
-
 public:
   enum class Graph {
     symmetricLaplacian,
@@ -28,21 +18,15 @@ public:
     adjacency
   };
 
-  constexpr static FuzzyCut fuzzy{};
-  constexpr static HardCut hard{};
-
   GraphCut() = default;
   GraphCut(std::vector<arma::mat> const &adjacencies,
            Graph type = Graph::symmetricLaplacian);
 
   WeightedClusters run(std::uint8_t nClusters,
                        std::uint16_t kmeans_iterations) const;
-  HardClusters run(std::uint8_t nClusters, HardCut) const;
 
   double calculateClustersScore(
       const std::vector<std::vector<bool>> &rawClusters) const;
-  double calculateClustersScore(const HardClusters &clusters) const;
-  template <typename Clusters> void setInitialClusters(Clusters &&clusters);
 
 private:
   inline arma::mat createGraph(const arma::mat &adjacency) const;
@@ -54,20 +38,11 @@ private:
   WeightedClusters partitionGraph(std::uint8_t nClusters,
                                   std::uint16_t kmeans_iterations, Fun graphFun,
                                   Gen &&random_generator) const;
-  template <typename Fun>
-  HardClusters partitionGraphHard(std::uint8_t nClusters, Fun graphFun) const;
-  template <typename Clusters>
-  double calculateCutScore(const arma::mat &graph,
-                           const Clusters &clusters) const;
-  template <typename ClusterA, typename ClusterB>
-  static double calculateCut(const arma::mat &graph, const ClusterA &clusterA,
-                             const ClusterB &clusterB);
 
   arma::mat getGraphWithNoLoops(const arma::mat &matrix) const;
 
   Graph graphType = Graph::symmetricLaplacian;
   std::vector<arma::mat> adjacencies;
-  std::variant<std::monostate, HardClusters, WeightedClusters> initialClusters;
 };
 
 arma::mat pairwise_distances(arma::mat const &a, arma::subview<double> b);

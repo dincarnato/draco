b732cdac73cbd8e79ece3d4c590d336a04425fba
Edoardo Morandi <morandidodo@gmail.com>
2025-11-18 10:16:30 +0100

    test: add for distances_size

--- test/clusters_replicates.cpp
+++ test/clusters_replicates.cpp
@@ -2,4 +2,11 @@
 
 using namespace clusters_replicates;
 
-int main() {}
+static void test_distances_size() {
+  assert(distances_size(1, 5) == 0);
+  assert(distances_size(2, 5) == 25);
+  assert(distances_size(4, 5) == 25 * 6);
+  assert(distances_size(6, 4) == 16 * 15);
+}
+
+int main() { test_distances_size(); }

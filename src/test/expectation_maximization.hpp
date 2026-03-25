aa24af04c0ca8d7249a74939ff88f0347a213753
Edoardo Morandi <morandidodo@gmail.com>
2026-03-25 12:32:04 +0100

    test: add variant of ExpectationMaximization
    In order to access protected fields from the tests.

---
+++ src/test/expectation_maximization.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "../expectation_maximization.hpp"
+#include "args.hpp"
+#include "compact_ringmap.hpp"
+#include "weighted_clusters.hpp"
+
+#include <random>
+#include <utility>
+
+namespace test {
+namespace expectation_maximization {
+
+struct ExpectationMaximization : ::ExpectationMaximization {
+  template <typename G>
+    requires std::uniform_random_bit_generator<std::remove_cvref_t<G>>
+  ExpectationMaximization(CompactRingmap const &ringmap,
+                          WeightedClusters &weights, Args const &args, G &&rng)
+      : ::ExpectationMaximization(ringmap, weights, args,
+                                  std::forward<G>(rng)) {}
+
+  double expectation() noexcept {
+    return ::ExpectationMaximization::expectation();
+  }
+
+  void maximization() noexcept { ::ExpectationMaximization::maximization(); }
+
+  constexpr auto responsibilities(this auto &self) -> decltype(auto) {
+    return (self.responsibilities_);
+  }
+
+  constexpr auto log_likelihoods(this auto &self) -> decltype(auto) {
+    return (self.log_likelihoods_);
+  }
+
+  constexpr auto weights_buffer(this auto &self) -> decltype(auto) {
+    return (self.weights_buffer_);
+  }
+
+  constexpr auto priors(this auto &self) -> decltype(auto) {
+    return (self.priors_);
+  }
+};
+
+} // namespace expectation_maximization
+
+using ExpectationMaximization =
+    expectation_maximization::ExpectationMaximization;
+
+} // namespace test
